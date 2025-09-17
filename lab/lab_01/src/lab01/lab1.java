9f6044e6a6a9d84c049cf1bdb2ecd10e16e3fa3b
guiguisocute_lap <g244629079@gmail.com>
2025-09-17 14:47:33 +0800

    push

--- lab/lab_01/src/lab01/lab1.java
+++ lab/lab_01/src/lab01/lab1.java
@@ -56,7 +56,7 @@ class request2{
     public void print_9x9chart() {
         for(int i = 1; i <= 9; i++){
             for(int j = 1; j <= i; j++){
-                System.out.print(i + "*" + j + "=" + (i*j) + "\t");     //试一下
+                System.out.print(i + "*" + j + "=" + (i*j) + "\t");
             }
             System.out.println();
         }
