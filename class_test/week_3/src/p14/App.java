214dacbd575285ac8a628b3eb3bfc8d7d240f4d4
guiguisocute_lap <g244629079@gmail.com>
2025-09-18 23:28:34 +0800

    add many files

---
+++ class_test/week_3/src/p14/App.java
@@ -0,0 +1,34 @@
+package p14;
+//编写方法 void append()，在顺序表末尾增加一组元素（数量未知）。当顺序表满时， 则自动扩容 1/2。
+
+/*
+输入为：
+8 4 3 0
+
+输出结果：
+La=1 3 5 6 9
+在末尾所加后，La=1 3 5 6 9 8 4 3
+ */
+public class App{
+
+    public static void main (String[] args) {
+
+//        SeqList La;
+//
+//        int a[]={1,3,5,6,9};
+//
+//        La=new SeqList(5,a);
+
+        //补充完整
+
+    }
+
+}
+
+class SeqList{          //顺序表结构=数组+表长
+
+//    int []a;   int len;
+
+    //补充完整
+
+}
