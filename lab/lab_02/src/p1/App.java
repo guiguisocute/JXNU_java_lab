fd7911cd8140f589520955b7b1544ffad1a82e7c
guiguisocute_lap <g244629079@gmail.com>
2025-09-20 00:35:34 +0800

    finishied lab02(may be will have some small fix in future)

--- lab/lab_02/src/p1/App.java
+++ lab/lab_02/src/p1/App.java
@@ -1,11 +1,12 @@
 package p1;
+
 /*
 一、构造顺序表，并满足如下要求：
 1、可以在创建顺序表对象时指定容量；若未指定容量，则按默认容量 10 创建顺序表；
 2、编写方法 void append()，在顺序表末尾增加一组元素（数量未知）。当顺序表满时，
 则自动扩容 1/2。如原容量 10，满时新容量为 15;
 3、编写方法 void show()，输出表信息；
-4、实现允许直接打印顺序表对象，即输出顺序表的所有元素；
+4、实现允许直接打印顺序表对象，即输出顺序表的所有元素；【重写父类toString()方法；】
 5、编写方法 void merge(y)：若调用 x.merge(y)，效果为：将 y 中所有元素插入到 x 的首
 部。
 
@@ -17,6 +18,117 @@ package p1;
 在测试类 main 中必须对每个方法进行测试，且有相关的提示信息
  */
 public class App {
+
+    public static void main(String[] args) {
+
+        SeqList x;
+        SeqList y;
+        int[] a = { 1, 2, 3, 4, 5 };
+        int[] b = {6,7,8,9};
+        x = new SeqList(5, a);
+        y = new SeqList(b);
+        x.show("x");
+        y.show("y");
+        SeqList xpy = new SeqList(5,a);
+        xpy.append(b);
+        xpy.show("append x + y后");
+
+        x.merge(y);
+        x.show("x merge y 后");
+
+
+        // 补充完整
+
+    }
+
+}
+
+class SeqList { // 顺序表结构=数组+表长
+
+    int[] a;
+    int len;
+
+    SeqList(int size, int[] b) {
+        a = new int[size];
+        len = b.length;
+
+        System.arraycopy(b, 0, a, 0, len);
+    }
+
+    SeqList(int[] b) {
+        len = 10;
+        a = new int[10];
+        len = b.length;
+
+        System.arraycopy(b, 0, a, 0, len);
+    }
+
+
+    void show(String s) {
+        System.out.println(s + "顺序表数据为：" + this +"; 表长为：" + len + "; 实际容量为" + a.length);
+    }
+
+    @Override
+    public String toString(){
+        String res = "";
+        int j = 0;
+        for(int i : this.a){
+            if(j < len)
+                res += i + " ";         //好像有更优的方法，但我懒得用了,只影响效率
+                j++;
+        }
+        return res;
+    }
+
+
+    void append(int[] b) {      //末尾加元素若不够size扩容一半
+        int j = len;
+        if((len + b.length) > a.length ){   // 数组有效数据还是得靠自己维护
+            int new_length = (int)(len + len * 0.5);
+            while (new_length < len + b.length) {
+                new_length = (int) (new_length + new_length * 0.5);
+            }
+            
+            int[] c = new int[new_length];
+            j = 0;
+            for(int i : a){
+                c[j] = i;
+                j++;
+            }
+            for(int i : b){
+                c[j] =i;
+                j++;
+            }
+            a = c;
+            len = j;
+        }else{
+            for(int i : b){
+                a[j] = i;
+                j++;
+            }
+        }
+    }
+
+    void merge(SeqList y){
+        int[] c = new int[len + y.len];
+        int j = 0;
+        for(int i : y.a){
+            if(j < y.len){
+                c[j] = i;
+                j++;
+            }
+
+        } 
+        for(int i : a){
+            if (j < len + y.len) {
+                c[j] = i;
+                j++;
+            }
+
+        }
+        a = c;
+        len = j;
+    }
 
 }
 
--- lab/lab_02/src/p4/App.java
+++ lab/lab_02/src/p4/App.java
@@ -1,8 +1,28 @@
 package p4;
+
 /*
 假设给定输入：1.2+3.4*（5.6-7.8/（9.0-10））。请借助正则表达式实现：（1）提取出
 所有的操作数，并输出； （2）提取出所有的运算符（注意左右括号也视为运算符）；
  */
-public class App {
 
+import java.util.regex.Matcher;
+
+public class App {
+    public static void main(String[] args) {
+        String str = "1.2+3.4*(5.6-7.8/(9.0-10))";
+        String regex1 = "\\d+(\\.)?\\d+"; //匹配操作数
+        String regex2 = "[+\\-*/()]"; //匹配运算符
+
+        System.out.println("操作数有：");
+        Matcher m1 = java.util.regex.Pattern.compile(regex1).matcher(str);
+        while (m1.find()) {
+            System.out.print(m1.group() + " ");
+        }
+        System.out.println("\n运算符有：");
+        Matcher m2 = java.util.regex.Pattern.compile(regex2).matcher(str);
+        while (m2.find()) {
+            System.out.print(m2.group() + " ");
+        }
+    }
 }
+
